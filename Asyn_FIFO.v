da82dd7c0ca9f661531171f15796471d1e60c6c3
tseal22 <144322456+tseal22@users.noreply.github.com>
2024-06-05 23:44:51 +0530

    Update Asyn_FIFO.v

--- Asyn_FIFO.v
+++ Asyn_FIFO.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 05.06.2024 22:41:06
+// Create Date: 02.06.2024 18:23:02
 // Design Name: 
-// Module Name: FIFO_tb
+// Module Name: Asyn_FIFO
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,72 +20,90 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module FIFO_tb();
-
-    // Parameters
-    parameter DATA_WIDTH = 8;
-
-  wire [DATA_WIDTH-1:0] data_out;
-  wire full;
-  wire empty;
-  reg [DATA_WIDTH-1:0] data_in;
-  reg w_en, wclk, wrst_n;
-  reg r_en, rclk, rrst_n;
-
-  // Queue to push data_in
-  reg [DATA_WIDTH-1:0] wdata_q, wdata;
-  integer i;
-
-  asynchronous_fifo as_fifo (wclk, wrst_n,rclk, rrst_n,w_en,r_en,data_in,data_out,full,empty);
-
-  always #10 wclk = ~wclk;
-  always #35 rclk = ~rclk;
-  
-  initial begin
-    wclk = 1'b0; wrst_n = 1'b0;
-    w_en = 1'b0;
-    data_in = 0;
-    
-    repeat(10) @(posedge wclk);
-    wrst_n = 1'b1;
-
-    repeat(2) begin
-      for (i=0; i<30; i++) begin
-        @(posedge wclk && !full);
-        w_en = (i%2 == 0)? 1'b1 : 1'b0;
-        if (w_en) begin
-          data_in = $urandom;
-          wdata_q.push_back(data_in);
-        end
-      end
-      #50;
-    end
-  end
-
-  initial begin
-    rclk = 1'b0; rrst_n = 1'b0;
-    r_en = 1'b0;
-
-    repeat(20) @(posedge rclk);
-    rrst_n = 1'b1;
-
-    repeat(2) begin
-      for ( i=0; i<30; i++) begin
-        @(posedge rclk && !empty);
-        r_en = (i%2 == 0)? 1'b1 : 1'b0;
-        if (r_en) begin
-          wdata = wdata_q.pop_front();
-          if(data_out !== wdata) $error("Time = %0t: Comparison Failed: expected wr_data = %h, rd_data = %h", $time, wdata, data_out);
-          else $display("Time = %0t: Comparison Passed: wr_data = %h and rd_data = %h",$time, wdata, data_out);
-        end
-      end
-      #50;
-    end
-
-    $finish;
-  end
-  
-  initial begin 
-    $dumpfile("dump.vcd"); $dumpvars;
-  end
+module Asyn_FIFO(wclk,rclk,wen,ren,wr,rd,rst,valid,empty,full,overflow,underflow);
+parameter WIDTH=8;
+parameter DEPTH=8;
+parameter addr=4;//one extra bit in msb to show Asynchronous FIFO is full or empty
+input wen;
+input ren;
+input wclk;
+input rclk;
+input  [DEPTH-1:0] wr;
+output reg [DEPTH-1:0] rd;
+input rst;
+output reg valid;
+output empty;
+output full;
+output reg overflow;
+output reg underflow;
+wire [addr-1:0] wpointer_g;
+wire [addr-1:0] rpointer_g;
+reg [addr-1:0] wpointer,wpointer_g_s1,wpointer_g_s2;
+reg [addr-1:0] rpointer,rpointer_g_s1,rpointer_g_s2;
+reg [WIDTH-1:0] mem[DEPTH-1:0];
+//write operation
+always@(posedge wclk)
+begin
+if(rst)
+wpointer<=0;
+else
+begin
+if(wen && !full)
+begin
+wpointer<=wpointer+1;
+mem[wpointer]<=wr;
+end  
+end  
+end 
+//read operation
+always@(posedge rclk)
+begin
+if(rst)
+rpointer<=0;
+else
+begin
+if(ren && !empty)
+begin
+rpointer<=rpointer+1;
+rd<=mem[rpointer];
+end  
+end  
+end 
+//read and write synchronizer
+assign wpointer_g=wpointer^(wpointer>>1);  
+assign rpointer_g=rpointer^(rpointer>>1);
+always@(posedge rclk)
+begin
+if(rst)
+begin
+wpointer_g_s1<=0;
+wpointer_g_s2<=0;
+end 
+else begin
+wpointer_g_s1 <= wpointer_g;
+wpointer_g_s2 <= wpointer_g_s1;
+end
+end 
+always@(posedge wclk)
+begin
+if(rst)
+begin
+rpointer_g_s1<=0;
+rpointer_g_s2<=0;
+end 
+else begin
+rpointer_g_s1 <= rpointer_g;
+rpointer_g_s2 <= rpointer_g_s1;
+end
+end
+assign empty=(rpointer_g==rpointer_g_s2);//empty condition
+assign full=(wpointer_g[addr-1]!= rpointer_g_s2[addr-1])//full condition
+&& (wpointer_g[addr-2]!= rpointer_g_s2[addr-2])
+&& (wpointer_g[addr-3]!= rpointer_g_s2[addr-3]);
+always@(posedge wclk) overflow<=full && wen;
+always@(posedge rclk)
+begin 
+underflow<=(empty && ren);//underflow condition
+valid<=(!empty && ren);//valid condition
+end
 endmodule
